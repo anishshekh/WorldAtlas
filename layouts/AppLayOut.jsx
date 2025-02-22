d84e4bade2f6feeae0824a17e6db3646b0ac3d47
anish shekh <anishshekh113@gmail.com>
2025-02-22 16:25:29 +0530

    delete navigate and useEffect

--- layouts/AppLayOut.jsx
+++ layouts/AppLayOut.jsx
@@ -1,16 +1,9 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import { useEffect } from "react";
+import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { ScrollTop } from "../components/ScrollTop";
 
 const AppLayOut = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/");
-  }, [navigate]);
-
   return (<>
     <ScrollTop />
     <Header />
