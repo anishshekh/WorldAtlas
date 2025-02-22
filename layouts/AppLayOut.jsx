483208099dbc3724b0d95e1bedcd55ece4ef986d
anish shekh <anishshekh113@gmail.com>
2025-02-22 15:46:23 +0530

    after refreshing redirect on home rout

--- layouts/AppLayOut.jsx
+++ layouts/AppLayOut.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useEffect } from "react";
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import  {ScrollTop}  from "../components/ScrollTop";
+import { ScrollTop } from "../components/ScrollTop";
 
 const AppLayOut = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, [navigate]);
 
   return (<>
     <ScrollTop />
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from '../pages/Home'
 import Contact from '../pages/Contact'
 import About from '../pages/About'
